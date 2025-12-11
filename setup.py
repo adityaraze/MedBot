60010a764190e97c9d70b048232fc9bbe3fa62bb
adityaraze <adityamodanwal293@gmail.com>
2025-12-11 18:52:59 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="MedBot",
+    version="0.1.0",
+    author="Aditya Modanwal",
+    author_email="adityamodanwal293@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
